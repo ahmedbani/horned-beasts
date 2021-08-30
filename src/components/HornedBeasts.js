596943c15786b3375528d938d4caffd992379d78
ahmedbani <a13banisal@gmail.com>
2021-08-30 15:08:26 +0300

    finished lab 2

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,35 @@
+import { template } from '@babel/core';
 import React from 'react';
+import { Card, Button, CardGroup, CardColumns } from 'react-bootstrap';
 
 class HornedBeasts extends React.Component {
-    render(){
-        return(
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    increment = () => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+
+    render() {
+        return (
             <>
-            <h2>{this.props.title}</h2>
-            <img src= {this.props.imageUrl}></img>
-            <p>{this.props.description}</p>
+                <Card style={{ width: '18rem' , height:'34rem', display: 'inline-grid', margin: ' 10px' }}>
+                    <Card.Img variant="top" src={this.props.imageUrl} onClick={this.increment} style={{height: '18rem'} }/>
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            👍🏼 : {this.state.counter} <br /> <br />
+                            {this.props.description}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+               
             </>
         );
     }
